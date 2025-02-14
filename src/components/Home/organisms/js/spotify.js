ce323dc00c9cd1afff40a2522ad4c4d3458f18c2
MelHellrigl <hellriglandreamelissa@gmail.com>
2025-02-14 00:52:42 -0300

    Update imgs

--- src/components/Home/organisms/js/spotify.js
+++ src/components/Home/organisms/js/spotify.js
@@ -3,15 +3,19 @@
     let navlist = document.querySelector(".sidebar");
     let containerNotif = document.querySelector(".showSideMenu");
     let headerr = document.querySelector("#headerr");
+    let footerr = document.querySelector("#footerr");
     
     menu.onclick = () => {
         menu.classList.toggle("bx-x");
         navlist.classList.toggle("openNav");
         if (navlist.classList.contains("openNav")) {
-            console.log('hola2')
             headerr.style.zIndex = "0";
         } else {
-            console.log('hola3')
             headerr.style.zIndex = "1";
+        }
+        if (navlist.classList.contains("openNav")) {
+            footerr.style.position = "static";
+        } else {
+            footerr.style.position = "relative";
         }
     };
